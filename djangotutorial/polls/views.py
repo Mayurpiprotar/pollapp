bc326cc862f5fe9f072a59f63e3bb4764544dda5
Mayurpiprotar <mhpiprotar109@gamil.com>
2026-02-13 21:00:48 +0530

    Lab 7: Completed Django Poll App with models, voting logic, templates and admin integration (Tutorial Part 3 & 4)

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,51 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
-from django.http import HttpResponse
-
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-
-# Leave the rest of the views (detail, results, vote) unchanged
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-from django.http import HttpResponse
-from django.template import loader
-
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
-
-from django.shortcuts import render
-
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -53,26 +10,27 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
-from django.http import Http404
-from django.shortcuts import render
 
-from .models import Question
-
-
-# ...
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
-
-from django.shortcuts import get_object_or_404, render
-
-from .models import Question
-
-
-# ...
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
